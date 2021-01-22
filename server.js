2d14880b55dc16d7d0263132be444ca9729c657b
HAMZAOUI Eya <g573116@sagemcom>
2021-01-22 15:07:52 +0100

    eya wejeh l9laya244sfp

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/paper-kit-2-angular'));
-
+//app.use(express.static(__dirname + '/dist/paper-kit-2-angular'));
+app.use(express.static('./dist/<npaper-kit-2-angular>'));
 // Send all requests to index.html
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/<paper-kit-2-angular'));
+// });
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/paper-kit-2-angular/index.html'));
-});
+    res.sendFile('index.html', {root: 'dist/<paper-kit-2-angular>/'}
+  );
+  });
 
 // default Heroku PORT
 app.listen(process.env.PORT || 8080);
